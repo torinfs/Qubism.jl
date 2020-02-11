8571a7c826657185b650700b358757ac3df807dc
Torin Stetina <torins@uw.edu>
2020-02-10 21:41:21 -0800

    bugfix, removed old gateset definition

--- src/Qubism.jl
+++ src/Qubism.jl
@@ -5,14 +5,10 @@ using LinearAlgebra
 include("register.jl")
 include("gates.jl")
 
-# Create primitive gate set
-g = Gates()
-
 # kron operator
 const ⊗ = kron
 
 # Export to user
-export g
 export ⊗
 
 end
